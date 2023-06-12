e7c2ad37163658ec4c09452d29efc6f0fe7cf7f0
Kwon <rynkwn@gmail.com>
2023-06-12 16:04:12 -0700

    Create a ... Xcode project I think.

---
+++ OSVT/OSVTApp.swift
@@ -0,0 +1,17 @@
+//
+//  OSVTApp.swift
+//  OSVT
+//
+//  Created by Ryan Kwon on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct OSVTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
